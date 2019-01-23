2d92d427d594f83a05736a2fd3910a7ca54abcb6
Po Wei Tsao <poweitsao@gmail.com>
2019-01-23 18:05:16 -0500

    new homepage

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -9,27 +9,43 @@ Widget build(context){
   return MaterialApp(
   home: Scaffold(
     body: Center(
-      child: ListView(
-        scrollDirection: Axis.vertical,
+        child: Container(
         padding: const EdgeInsets.all(35.0),
+        child: Column(
+        //scrollDirection: Axis.vertical,
         children: [
+          Icon(Icons.people,
+          size: 220.0,),
+          Container(
+            alignment: Alignment(0, -0.2),
+            padding: EdgeInsets.all(10.0),
+            child: Text('Welcome to MUN Helper!',
+            style: TextStyle(
+              color: textColor,
+              fontSize: 27.0,
+              //fontStyle:
+              )
+            ),
+          ),
           MenuButton(500.0, 100.0, 'MUN Resolution Help'),
-          MenuButton(500.0, 100.0, 'In-Conference Help')
+          MenuButton(500.0, 100.0, 'In-Conference Help'),
+          MenuButton(500.0, 100.0, 'Trolling Guide')
           ]
         )
-      ),
+      )
+    ),
 
     backgroundColor: bgColor,
     appBar: AppBar(
       //elevation: 0.0,
       titleSpacing: 5.0,
-      title: Text(
+      /*title: Text(
         'MUN Helper',
         style: TextStyle(
           color: textColor,
           fontSize: 27.0
         ), 
-      ),
+      ),*/
       backgroundColor: barColor
     ),
   )
--- lib/src/colors.dart
+++ lib/src/colors.dart
@@ -5,4 +5,4 @@ final buttonColor = const Color(0xFFD8C3A5);
 final bgColor = const Color(0xFFEAE7DC);
 //inal bgColor = const Color(0xFF8E8D8A);
 
-final textColor = const Color(0xFFE85A4F);
+final textColor = const Color(0xFF001126);
